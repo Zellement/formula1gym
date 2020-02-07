e8bdaec83b39b8c44ee64583e6140073f99d75f2
[Dan Farrow] <dan@zellement.com>
2020-02-07 21:55:18 +0000

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,13 +55,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `formula1gym`,
+        short_name: `formula1gym`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/f1-icon.png`,
       },
     },
   ],
