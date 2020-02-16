161a15f0664a53f811055cd9a688e72d09f743b6
[Dan Farrow] <dan@zellement.com>
2020-02-16 15:06:38 +0000

    Add aria label for logo

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = ({ siteTitle }) => (
     <header className="p-6 lg:py-12">
       <div className="container lg:flex lg:space-between">
         <div className="flex flex-wrap items-center">
-          <Link name="Go to the homepage" to="/">
+          <Link aria-label="Go to the homepage" name="Go to the homepage" to="/">
             <Logo />
           </Link>
         </div>
