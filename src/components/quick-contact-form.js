4b9f6ce3e3888414a23fa9d3344b54405074d219
[Dan Farrow] <dan@zellement.com>
2020-03-03 21:06:31 +0000

    Add validation to forms

--- src/components/quick-contact-form.js
+++ src/components/quick-contact-form.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { navigateTo } from "gatsby-link";
+import SimpleReactValidator from "simple-react-validator"
+import { navigateTo } from "gatsby-link"
 
 function encode(data) {
   return Object.keys(data)
@@ -10,32 +11,52 @@ function encode(data) {
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      email: "",
+      telephone: ""
+    };
+    this.validator = new SimpleReactValidator({
+      className: "absolute z-20 top-0 right-0 text-5xl w-32 h-32 bg-red-800 rounded-full leading-none -mt-1 -mr-1 text-yellow-light p-1 px-2",
+      messages: {
+        default: '*'
+      }
+    });
   }
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+
   handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
+    if (this.validator.allValid()) {
+      const form = e.target;
+      this.setState({
+        submitting: true
       })
-    })
+
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": form.getAttribute("name"),
+          ...this.state
+        })
+      })
       .then(() => navigateTo(form.getAttribute("action")))
       .catch(error => alert(error));
+    } else {
+      this.validator.showMessages();
+      this.forceUpdate();
+    }
+    e.preventDefault();
   };
 
   render() {
     return (
         <form
-          className="form"
+          className="form text-black"
           name="quick-contact"
           method="post"
           action="/thanks"
@@ -50,9 +71,18 @@ export default class Contact extends React.Component {
                 <label className="hidden">Do not fill this in<input name="bot-field" /></label>
             {/* </p> */}
             <div className="flex flex-wrap justify-between">
-              <label className="flex flex-1 m-1"><span className="hidden">Name</span><input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Name" type="text" name="name" onChange={this.handleChange} /></label>
-              <label className="flex flex-1 m-1"><span className="hidden">Email</span><input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Email"  type="email" name="email" onChange={this.handleChange} /></label>
-              <label className="flex flex-1 m-1"><span className="hidden">Telephone</span><input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Telephone" type="text" name="telephone" onChange={this.handleChange} /></label>
+              <label className="flex flex-1 m-1 relative"><span className="hidden">Name</span>
+                <input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Name" type="text" name="name" onChange={this.handleChange} />
+                {this.validator.message('name', this.state.name, 'required')}
+              </label>
+              <label className="flex flex-1 m-1 relative"><span className="hidden">Email</span>
+                <input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Email"  type="email" name="email" onChange={this.handleChange} />
+                {this.validator.message('name', this.state.email, 'required|email')}
+              </label>
+              <label className="flex flex-1 m-1 relative"><span className="hidden">Telephone</span>
+                <input className="bg-gray-300 flex-grow p-4 rounded-full" placeholder="Telephone" type="text" name="telephone" onChange={this.handleChange} />
+               {this.validator.message('name', this.state.telephone, 'required')}
+              </label>
               <button className="btn btn-yellow text-orange-semidark p-0 flex-1 m-1" type="submit"><span className="whitespace-no-wrap block p-4">Book induction</span></button>
             </div>
         </form>
