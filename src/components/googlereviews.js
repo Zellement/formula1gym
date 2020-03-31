3c42877211b70b073252c3fd9d0473fd9c020ee5
[Dan Farrow] <dan@zellement.com>
2020-03-31 20:45:23 +0100

    fix maps

--- src/components/googlereviews.js
+++ src/components/googlereviews.js
@@ -1,28 +1,28 @@
-import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+// import React from "react"
+// import { graphql, useStaticQuery } from "gatsby"
 
-const Reviews = () => {
-  const data = useStaticQuery(graphql`
-    query ReviewsQuery {
-      allGooglePlacesReview {
-        edges {
-          node {
-            id
-            author_name
-            relative_time_description
-            text
-            rating
-          }
-        }
-      }
-    }
-  `)
+// const Reviews = () => {
+//   const data = useStaticQuery(graphql`
+//     query ReviewsQuery {
+//       allGooglePlacesReview {
+//         edges {
+//           node {
+//             id
+//             author_name
+//             relative_time_description
+//             text
+//             rating
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  return (
-    <div className="w-full">
-      {data.allGooglePlacesReview.edges[0].node.text}
-    </div>
-  )
-}
+//   return (
+//     <div className="w-full">
+//       {data.allGooglePlacesReview.edges[0].node.text}
+//     </div>
+//   )
+// }
 
-export default Reviews
+// export default Reviews
