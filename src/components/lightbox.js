36940db8e08a3ac7271427dfa8ce55e3654f45d8
[Dan Farrow] <dan@zellement.com>
2020-04-01 15:03:02 +0100

    Update meet team for desktop

--- src/components/lightbox.js
+++ src/components/lightbox.js
@@ -14,11 +14,15 @@ export default function Lightbox({image, name, role, bio, children}) {
 			<FsLightbox
 				toggler={ toggler }
 				customSources={[
-					<div className="text-white text-left overflow-y-scroll p-8">
-						<p className="font-sans text-2xl">{name}</p>
-						<p className="font-sans text-xl text-orange">{role}</p>
-						<Img alt={image.alt} className="my-4 block w-full max-w-full h-auto" fixed={image} />
-						<HTMLContent className="text-sm font-sans" content={bio} />
+					<div className="text-white overflow-y-auto p-8 max-w-lg lg:max-w-3xl lg:overflow-hidden lg:flex">
+						<div className="">
+						<p className="font-sans text-2xl mx-auto lg:text-right">{name}</p>
+						<p className="font-sans text-xl text-orange mx-auto lg:text-right">{role}</p>
+						<Img alt={image.alt} className="my-4 block w-full max-w-full h-auto mx-auto" fixed={image} />
+						</div>
+						<div>
+						<HTMLContent className="content text-sm font-sans text-left lg:p-8 lg:mt-12" content={bio} />
+						</div>
 					</div>
 				]}
 			/>
