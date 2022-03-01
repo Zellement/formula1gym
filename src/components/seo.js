f78c5e628d4e582a02b18d0653a0a1e7fee62664
Dan Farrow <dan@zellement.com>
2022-03-01 10:47:37 +0000

    add schema

--- src/components/seo.js
+++ src/components/seo.js
@@ -22,176 +22,101 @@ function SEO({ description, lang, meta, title }) {
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
   const ogImage = file.publicURL
+
+  const schemaOrgWebPage = {
+    "@context": "https://schema.org",
+
+    "@type": "ExerciseGym",
+
+    name: "Formula One Gym",
+
+    image:
+      "https://images.prismic.io/formula1gym/aaed51bf-69a8-4d86-ace4-966ea70c04de_Incline-Bench-Press.jpg",
+
+    url: "https://www.formulaonegym.co.uk/",
+
+    telephone: "0115 950 5009",
+
+    email: " enquiries@formulaonegym.co.uk",
+
+    priceRange: "£",
+
+    address: {
+      "@type": "PostalAddress",
+
+      streetAddress: "21 Victoria Street",
+
+      addressLocality: "Nottingham",
+
+      postalCode: "NG1 2EW",
+
+      addressCountry: "GB",
+    },
+
+    geo: {
+      "@type": "GeoCoordinates",
+
+      latitude: 52.95345,
+
+      longitude: -1.14585,
+    },
+
+    openingHoursSpecification: [
+      {
+        "@type": "OpeningHoursSpecification",
+
+        dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday"],
+
+        opens: "06:30",
+
+        closes: "21:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+
+        dayOfWeek: "Friday",
+
+        opens: "06:30",
+
+        closes: "20:00",
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+
+        dayOfWeek: ["Saturday", "Sunday"],
+
+        opens: "09:00",
+
+        closes: "18:00",
+      },
+    ],
+
+    sameAs: [
+      "https://www.facebook.com/Formula-One-Gym-161546613946894/",
+
+      "https://www.instagram.com/formulaonegym/",
+
+      "http://search.google.com/local/writereview?placeid=ChIJdXtvmdXDeUgR877Bo28YMYU",
+    ],
+  }
 
   //console.log(ogImage)
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:image`,
-          content: ogImage,
-        },
-        {
-          property: `image`,
-          content: ogImage,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: 'google-site-verification',
-          content: "IfBKapJI5_7VN39JR1PPGt-6QCbnEcxseAhIGMLqD6w" 
-        }, 
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    >
-    {`
-    
-    {
-<script type="application/ld+json">
-
-{
-
-"@context": "https://schema.org",
-
-"@type": "ExerciseGym",
-
-"name": "Formula One Gym",
-
-"image": "https://images.prismic.io/formula1gym/aaed51bf-69a8-4d86-ace4-966ea70c04de_Incline-Bench-Press.jpg",
-
-"url": "https://www.formulaonegym.co.uk/",
-
-"telephone": "0115 950 5009",
-
-"email" :" enquiries@formulaonegym.co.uk",
-
-"priceRange": "£",
-
-"address": {
-
-"@type": "PostalAddress",
-
-"streetAddress": "21 Victoria Street",
-
-"addressLocality": "Nottingham",
-
-"postalCode": "NG1 2EW",
-
-"addressCountry": "GB"
-
-},
-
-"geo": {
-
-"@type": "GeoCoordinates",
-
-"latitude": 52.95345,
-
-"longitude": -1.14585
-
-},
-
-"openingHoursSpecification": [{
-
-"@type": "OpeningHoursSpecification",
-
-"dayOfWeek": [
-
-"Monday",
-
-"Tuesday",
-
-"Wednesday",
-
-"Thursday"
-
-],
-
-"opens": "06:30",
-
-"closes": "21:00"
-
-},{
-
-"@type": "OpeningHoursSpecification",
-
-"dayOfWeek": "Friday",
-
-"opens": "06:30",
-
-"closes": "20:00"
-
-},{
-
-"@type": "OpeningHoursSpecification",
-
-"dayOfWeek": [
-
-"Saturday",
-
-"Sunday"
-
-],
-
-"opens": "09:00",
-
-"closes": "18:00"
-
-}],
-
-"sameAs": [
-
-"https://www.facebook.com/Formula-One-Gym-161546613946894/",
-
-"https://www.instagram.com/formulaonegym/",
-
-"http://search.google.com/local/writereview?placeid=ChIJdXtvmdXDeUgR877Bo28YMYU"
-
-]
-
-}
-
-</script>
-
-    }
-
-    `}
+    <Helmet title={title}>
+      <html lang={lang} />
+      <meta name="description" content={description} />
+      <meta name="image" content={ogImage} />
+      <meta name="formula-one-gym" content="Formula One Gym" />
+      {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
+      {
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgWebPage)}
+        </script>
+      }
+      {/* {article && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>}
+        <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script> */}
     </Helmet>
   )
 }
