82a5499e06aef041e46a8f864ddd5b10352e246c
Dan Farrow <dan@zellement.com>
2023-01-02 10:11:36 +0000

    update safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,86 +1,81 @@
 module.exports = {
-  purge: [
-    './src/**/*.js',
-    './src/**/*.css',
-  ],
+  purge: {
+    content: ["./src/**/*.js", "./src/**/*.css"],
+    safelist: ["md:order-1", "md:order-0"],
+  },
   theme: {
     minHeight: {
-      '0': '0',
-      '1/2': '50%',
-      '1/4': '25%',
-      '3/4': '75%',
-      'full': '100%',
-      '250': '250px',
-      '200': '200px',
-      '150': '150px',
-      '42': '42px',
-      '80': '80px'
+      0: "0",
+      "1/2": "50%",
+      "1/4": "25%",
+      "3/4": "75%",
+      full: "100%",
+      250: "250px",
+      200: "200px",
+      150: "150px",
+      42: "42px",
+      80: "80px",
     },
     extend: {
       padding: {
-        '150': '150px'
+        150: "150px",
       },
       margin: {
-        '-150': '-150px'
+        "-150": "-150px",
       },
       width: {
-        '32': '32px',
-        '50': '50px',
-        '75': '75px'
+        32: "32px",
+        50: "50px",
+        75: "75px",
       },
       height: {
-        '32': '32px',
-        '50': '50px',
-        '75': '75px'
+        32: "32px",
+        50: "50px",
+        75: "75px",
       },
       minWidth: {
-        '50': '50px',
-        '75': '75px',
-        '400': '400px'
+        50: "50px",
+        75: "75px",
+        400: "400px",
       },
       maxWidth: {
-        '80': '80px',
-        '90': '90px',
-        '200': '200px',
-        '300': '300px'
+        80: "80px",
+        90: "90px",
+        200: "200px",
+        300: "300px",
       },
       fontSize: {
-        '7xl': '5em',
-        '8xl': '6em',
-        '9xl': '7em',
-        '10xl': '8em',
+        "7xl": "5em",
+        "8xl": "6em",
+        "9xl": "7em",
+        "10xl": "8em",
       },
       fontFamily: {
-        sans: [
-          'Raleway', 'sans-serif'
-        ],
-        display: [
-          'Sedgwick\\ Ave', 'serif'
-        ]
+        sans: ["Raleway", "sans-serif"],
+        display: ["Sedgwick\\ Ave", "serif"],
       },
       colors: {
         blue: {
-          default: '#00A3FF',
-          dark: '#0077b7'
+          default: "#00A3FF",
+          dark: "#0077b7",
         },
         orange: {
-          light: '#fcf2e3',
-          default: '#FF9900',
-          semidark: '#AD5E00',
+          light: "#fcf2e3",
+          default: "#FF9900",
+          semidark: "#AD5E00",
         },
         yellow: {
-          default: '#FFC700',
-          light: '#ffe07a'
+          default: "#FFC700",
+          light: "#ffe07a",
         },
         hotpink: {
-          "500": '#E8005A',
-          "700": '#C2004A',
+          500: "#E8005A",
+          700: "#C2004A",
         },
       },
-    }
+    },
   },
   corePlugins: {
-    container: false
-  }
+    container: false,
+  },
 }
-
